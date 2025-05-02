33f2ac65cda0c20238e2cdbee8887bdd96c05c65
Akhil-design-jpg <vakhil838@gmail.com>
2025-05-02 12:28:57 +0530

    second

--- 01_basics/hello.py
+++ 01_basics/hello.py
@@ -10,3 +10,12 @@ chai(4)
 chai_one = "lemon tea"
 chai_two = "ginger tea"
 chai_three = "masala chai"
+
+
+def percent_calculate(number,whole):
+    percentage = (number/whole)*100
+    return f"{percentage:.2f}%"
+# f to give answer in float(decimal)
+# 2f - 2 float (2 decimal units)
+result = percent_calculate(20,200)
+print(result)
