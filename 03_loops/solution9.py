abd642da4a2706bda4ab2bddccaf7fe23ce4bad1
Akhil_Verma <74674142+Akhil-design-jpg@users.noreply.github.com>
2025-05-08 11:07:44 +0530

    Update solution9.py

--- 03_loops/solution9.py
+++ 03_loops/solution9.py
@@ -1,1 +1,11 @@
-print("Hello")
+items = ["apple","banana","orange","apple","mango"]
+
+
+unique_item = set()
+
+
+for item in items:
+    if item in unique_item:
+        print("Duplicate",item)
+        break
+    unique_item.add(item)    
