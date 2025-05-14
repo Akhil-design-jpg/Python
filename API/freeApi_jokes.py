889d799b6f497b19828f84f4e9a244e9db92d5d5
Akhil_Verma <74674142+Akhil-design-jpg@users.noreply.github.com>
2025-05-14 12:23:07 +0530

    Update freeApi_jokes.py

--- API/freeApi_jokes.py
+++ API/freeApi_jokes.py
@@ -1,41 +1,32 @@
 import requests
 
-def fetchFreeAPI_comment():
-    url = "https://api.freeapi.app/api/v1/public/youtube/comments/cv-6bAeYsOY"
+def fetch_jokeApi():
+    url = "https://api.freeapi.app/api/v1/public/randomjokes/joke/random"
     response = requests.get(url)
     data = response.json()
     
     
-    if data.get('success') and 'data' in data:
+    if ["success"] and "data" in data:
         user_data = data["data"]
+        message = user_data.get("message",["message"])
+        content = user_data.get("content",["content"])
         
-        # if not isinstance(user_data,list):
-        #     raise Exception("Expected a list of comments, but got something else")
-        
-        
-    for user_data1 in user_data:
-        videoId = user_data1.get("videoId","videoId")
-        comment_id = user_data1.get("comment_id","comment_id")
-        snippet = user_data1.get("snippet","snippet")
-        LikeCount = user_data1.get("likeCount","likeCount")
-        publishedAt = user_data1.get("publishedAt","publishedAt")
-        totalReplyCount = user_data1.get("totalreplyCount","totalreplyCount")
-        
-        print(f"Video ID: {videoId} \n CommentID: {comment_id} \nSnippet: {snippet}")
-        print(f"Likes: {LikeCount} \n PublishedDate: {publishedAt} \n Replies: {totalReplyCount}")
-    
+        return message, content
     
     else:
-        raise Exception("Failed to fetch")
+        raise Exception("failed to fetch the data")
+    # 
     
-
+    
 def main():
+    
     try:
-        video_id, comment_id, snippet, like_count, published_at, total_reply_count = fetchFreeAPI_comment()
-        print(f"Video ID: {video_id}\n \n Comment ID: {comment_id}\n \n Snippet: {snippet}\n")
-        print(f"Likes: {like_count}\nPublished At: {published_at}\nReplies: {total_reply_count}")
+        message, content = fetch_jokeApi()
+        print(f"Message: {message} \n Content: {content}")
     except Exception as err:
-        print("Some error occurred:", err)
-        # 
+        print(str(err))
+        print(err)
+        
 if __name__ == "__main__":
     main()
+    
